58f3ae22037ecf5e7327712bfbdf692c84b0232f
EldHasp <EldHasp.WpfMvvm@Gmail.com>
2020-11-28 20:16:05 +0300

    Добавлен пример для ExpressionConverter

---
+++ WpfMvvm.Examples/WpfMvvm.Converters.Examples/Expression/ExpressionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfMvvm.Converters.Examples.Expression
+{
+    /// <summary>
+    /// Логика взаимодействия для ExpressionWindow.xaml
+    /// </summary>
+    public partial class ExpressionWindow : Window
+    {
+        public ExpressionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
